74132c608b66e976723b37f4b3dc4a668a42703d
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-09 21:18:49 +0200

    updated

--- Django_TodoApp/urls.py
+++ Django_TodoApp/urls.py
@@ -20,4 +20,6 @@ from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('addTodo', views.addTodo),
+    path('update/<int:id>', views.update),
 ]
